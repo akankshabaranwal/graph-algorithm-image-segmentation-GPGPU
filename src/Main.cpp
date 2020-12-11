1bce73b73fafdbc80e3a294482f2bc9c5eadc0e3
Gyorgy Rethy <rethygy0410@gmail.com>
2020-12-11 16:21:30 +0100

    [dev] Add command line arguments

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,12 +5,21 @@
 
 #include "mst.h"
 
+void printUsage() {
+    puts("Usage: ./felz [input image path] [output image path]");
+}
+
 int main(int argc, char **argv)
 {
+    if (argc < 3) {
+        printUsage();
+        exit(1);
+    }
+
     cv::Mat image, output;
     cv::cuda::GpuMat dev_image, dev_output;
 
-    image = imread("data/beach.png", cv::IMREAD_COLOR);
+    image = imread(argv[1], cv::IMREAD_COLOR);
     std::cout << "Computing on: " << image.rows << "x" << image.cols << std::endl;
     dev_image.upload(image);
 
@@ -19,10 +28,10 @@ int main(int argc, char **argv)
 
     dev_output.download(output);
 
-    namedWindow("Source Image", cv::WINDOW_NORMAL);
-    imshow("Source Image", image);
-    namedWindow("After Blur (CUDA)", cv::WINDOW_NORMAL);
-    imshow("After Blur (CUDA)", output);
+    //namedWindow("Source Image", cv::WINDOW_NORMAL);
+    //imshow("Source Image", image);
+    //namedWindow("After Blur (CUDA)", cv::WINDOW_NORMAL);
+    //imshow("After Blur (CUDA)", output);
 
     time_t start, end;
     time(&start);
@@ -33,11 +42,11 @@ int main(int argc, char **argv)
     std::cout << "Segmentation time: " << double(end - start) << std::endl;
 
     cv::Mat output_img = cv::Mat(image.rows, image.cols, CV_8UC3, segmented_img);
-    namedWindow("Segmented", cv::WINDOW_NORMAL);
-    imshow("Segmented", output_img);
+    //namedWindow("Segmented", cv::WINDOW_NORMAL);
+    //imshow("Segmented", output_img);
     cv::waitKey();
 
-    imwrite("segmented.png", output_img);
+    imwrite(argv[2], output_img);
 
     return 0;
 }
