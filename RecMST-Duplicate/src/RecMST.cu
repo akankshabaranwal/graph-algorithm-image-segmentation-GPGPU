d1b88b469e8f4b6bc348d9169156b51f25d0564a
ahoste <ahoste@student.ethz.ch>
2020-12-16 12:17:11 +0100

    add

--- RecMST-Duplicate/src/RecMST.cu
+++ RecMST-Duplicate/src/RecMST.cu
@@ -641,7 +641,7 @@ void writeComponents(std::vector<unsigned int*>& d_hierarchy_levels, std::vector
 		std::string outfilename = rawOutName + std::string("_")  + std::to_string(l) + std::string(".png");
 		std::string outmessage = std::string("Writing ") + outfilename.c_str() + std::string("\n");
 
-		fprintf(stderr, outmessage.c_str());
+		fprintf(stderr, "%s", outmessage);
 		imwrite(outfilename, output_img);
 	}
 
