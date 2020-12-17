181d7a2ea929253dcc96bcb3f0fd1931b77182a0
ahoste <ahoste@student.ethz.ch>
2020-12-17 21:01:36 +0100

    debug

--- RecMST-Duplicate/src/Kernels.cu
+++ RecMST-Duplicate/src/Kernels.cu
@@ -493,13 +493,8 @@ __global__ void AppendKernel_1(unsigned long long int *d_segmented_min_scan_inpu
 
 
 
-        /*if (tid < d_edges[tid]) {
-            val = val<<MOVEBITS; // TODO
+        val = val<<MOVEBITS; // TODO
             val = val|tid; // TODO
-        } else {
-            val = val<<MOVEBITS; // TODO
-            val = val|d_edges[tid]; // TODO
-        }*/
         
 
 		val=val<<MOVEBITS;
