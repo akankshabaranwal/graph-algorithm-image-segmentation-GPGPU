8a25d89c075efc1598adc22cfaf8f0ce5d9032c6
ahoste <ahoste@student.ethz.ch>
2021-01-13 18:50:38 +0100

    debug

--- RecMST-Duplicate/src/Kernels.cu
+++ RecMST-Duplicate/src/Kernels.cu
@@ -43,9 +43,9 @@ __global__ void createAvgColorArray(unsigned char *image, float *d_avg_color_r, 
         unsigned int this_g = (unsigned int) image[this_img_idx + 1];
         unsigned int this_b = (unsigned int) image[this_img_idx + 2];
         unsigned int write_idx = row * no_of_cols + col;
-        d_avg_color_r[write_idx] = __uint_as_float(this_r);
-        d_avg_color_g[write_idx] = __uint_as_float(this_g);
-        d_avg_color_b[write_idx] = __uint_as_float(this_b);
+        d_avg_color_r[write_idx] = 0.0 + this_r;
+        d_avg_color_g[write_idx] = 0.0 + this_g;
+        d_avg_color_b[write_idx] = 0.0 + this_b;
         printf("%u\n", this_r);
     }
 }
